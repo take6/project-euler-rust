adfa2e367fc09d32d29775c8e5338212b5843fb9
take6 <takeshi.nakazato.naoj@gmail.com>
2022-12-03 11:45:51 +0900

    Problem #5 remove code for debugging

--- 0005/evendiv/src/main.rs
+++ 0005/evendiv/src/main.rs
@@ -7,9 +7,6 @@ fn main() {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
         11, 12, 13, 14, 15, 16, 17, 18, 19, 20
     ];
-    // const BASE_NUMBERS: [u64; 10] = [
-    //     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    // ];
 
     let mut counter: [u64; BASE_NUMBERS.len()] = [0; BASE_NUMBERS.len()];
 
